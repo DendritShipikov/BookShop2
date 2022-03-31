720715e73cb81ac73849203e8c47827ec6204a6d
Dendrit Shipikov <sma2003@yandex.by>
2022-03-31 15:33:41 +0300

    added swagger docs to userapi

--- user-api/src/main/java/com/dendrit/bookshop/userapi/UserApiApplication.java
+++ user-api/src/main/java/com/dendrit/bookshop/userapi/UserApiApplication.java
@@ -3,13 +3,17 @@ package com.dendrit.bookshop.userapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@Configuration
+@EnableWebMvc
+@EnableSwagger2
 public class UserApiApplication {
 
     public static void main(String[] args) {
@@ -19,6 +23,14 @@ public class UserApiApplication {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.dendrit.bookshop.userapi.controllers"))
+                .build();
     }
 
 }
--- user-api/src/main/java/com/dendrit/bookshop/userapi/controllers/UserController.java
+++ user-api/src/main/java/com/dendrit/bookshop/userapi/controllers/UserController.java
@@ -5,6 +5,10 @@ import com.dendrit.bookshop.common.data.UserLoginForm;
 import com.dendrit.bookshop.common.data.UserRegistrationForm;
 import com.dendrit.bookshop.userapi.exceptions.IncorrectPasswordException;
 import com.dendrit.bookshop.userapi.services.UserService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,19 +25,37 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Get user by id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Return user"),
+            @ApiResponse(code = 404, message = "User with such username does not exist")
+    })
     @GetMapping("/{id}")
-    public UserData getById(@PathVariable Long id) {
+    public UserData getById(@Parameter(description = "Id of user to be selected") @PathVariable Long id) {
         return userService.getUserById(id);
     }
 
+    @Operation(summary = "User registration")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "User registration"),
+            @ApiResponse(code = 422, message = "User with such username is already exists")
+    })
     @PostMapping("/register")
-    public ResponseEntity<UserData> register(@RequestBody UserRegistrationForm registrationForm) {
+    public ResponseEntity<UserData> register(
+            @Parameter(description = "Registration form") @RequestBody UserRegistrationForm registrationForm) {
         UserData userData = userService.registration(registrationForm);
         return ResponseEntity.status(HttpStatus.CREATED).body(userData);
     }
 
+    @Operation(summary = "Get token")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Return page of books"),
+            @ApiResponse(code = 401, message = "Incorrect password"),
+            @ApiResponse(code = 404, message = "User with such username does not exist")
+    })
     @PostMapping("/token")
-    public ResponseEntity<String> generateToken(@RequestBody UserLoginForm loginForm) throws IncorrectPasswordException {
+    public ResponseEntity<String> generateToken(
+            @Parameter(description = "Username-password pair")@RequestBody UserLoginForm loginForm) throws IncorrectPasswordException {
         String token = userService.generateToken(loginForm);
         return ResponseEntity.ok().body(token);
     }
