c50d172a76543ae9ec6fa87b4558bc5d8b4817ba
Dendrit Shipikov <sma2003@yandex.by>
2022-04-13 12:53:13 +0300

    added smth

--- book-api/src/main/java/com/dendrit/bookshop/bookapi/BookApiApplication.java
+++ book-api/src/main/java/com/dendrit/bookshop/bookapi/BookApiApplication.java
@@ -1,17 +1,17 @@
 package com.dendrit.bookshop.bookapi;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Properties;
 
 @SpringBootApplication
 @EnableWebMvc
@@ -33,6 +33,11 @@ public class BookApiApplication {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.dendrit.bookshop.bookapi.controllers"))
                 .build();
+    }
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("audit").partitions(2).replicas(1).build();
     }
 
 }
--- book-api/src/main/java/com/dendrit/bookshop/bookapi/aspects/CalculateTimeAspect.java
+++ book-api/src/main/java/com/dendrit/bookshop/bookapi/aspects/CalculateTimeAspect.java
@@ -3,9 +3,10 @@ package com.dendrit.bookshop.bookapi.aspects;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,15 +15,21 @@ public class CalculateTimeAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CalculateTimeAspect.class);
 
-    @Pointcut("execution(* com.dendrit.bookshop.bookapi.controllers.*.*(..))")
-    public void inControllerLayer() {}
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public void setKafkaTemplate(KafkaTemplate kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
 
     @Around("@annotation(com.dendrit.bookshop.bookapi.aspects.CalculateTime)")
     public Object aroundAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         long startTime = System.currentTimeMillis();
         Object result = proceedingJoinPoint.proceed();
         long endTime = System.currentTimeMillis();
-        LOGGER.info("Method '" + proceedingJoinPoint.getSignature() + "' takes " + (endTime -startTime) + "ms");
+        String msg = "Method '" + proceedingJoinPoint.getSignature() + "' takes " + (endTime -startTime) + "ms";
+        LOGGER.info(msg);
+        kafkaTemplate.send("audit", msg);
         return result;
     }
 
